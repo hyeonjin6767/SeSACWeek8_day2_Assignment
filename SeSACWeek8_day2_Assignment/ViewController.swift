a5b49f1629db41acaad1b1f16c9d1474abdd3205
hyeonjin6767 <parkhj067670@gmail.com>
2025-08-19 15:51:38 +0900

    Initial Commit

---
+++ SeSACWeek8_day2_Assignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeSACWeek8_day2_Assignment
+//
+//  Created by 박현진 on 8/19/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
